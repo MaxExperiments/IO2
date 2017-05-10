34e4909999618d6991779e191d2d8b797616c329
MaxExperiments <maxime.flin@gmail.com>
2017-05-10 18:20:11 +0200

    ajoute une image sur l'index des posts

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -9,19 +9,24 @@
     </div>
     <?php foreach($posts as $post): ?>
         <article class="post" data-id="<?= $post->id ?>" data-ressource-type="posts">
-            <div class="lead">
-                <?= $Html->route('<h2>'. $post->title .'</h2>', 'PostsController@show',['id'=>$post->id]) ?>
-                <p>Un post de <strong><a href="/users/<?= $post->user_id ?>"><?= $post->pseudo ?></a></strong> le <?= date('j/m/Y', strtotime(($post->updated_at) ? $post->updated_at : $post->created_at)) ?></p>
+            <div class="post-photo">
+                <img src="<?= ($post->photo!==null) ? $post->photo : '/assets/imgs/default-post-img.gif' ?>" alt="">
             </div>
-            <p><?= $Html->shortCut($post->content) ?>...</p>
-            <div class="link">
-                <?= $Html->route('Voir la suite', 'PostsController@show',['id'=>$post->id]) ?>
-                <?php if (Session::isAuthenticate()): ?>
-                    <?php $Html->addScript('/assets/js/app.js') ?>
-                    <?php if (Session::Auth()->id == $post->user_id) echo $Html->route('Détruire', 'PostsController@destroy',
-                                    ['id'=>$post->id],
-                                    ['class'=>'button alert del-button']); ?>
-                <?php endif ?>
+            <div class="post-content">
+                <div class="lead">
+                    <?= $Html->route('<h2>'. $post->title .'</h2>', 'PostsController@show',['id'=>$post->id]) ?>
+                    <p>Un post de <strong><a href="/users/<?= $post->user_id ?>"><?= $post->pseudo ?></a></strong> le <?= date('j/m/Y', strtotime(($post->updated_at) ? $post->updated_at : $post->created_at)) ?></p>
+                </div>
+                <p><?= $Html->shortCut($post->content) ?>...</p>
+                <div class="link">
+                    <?= $Html->route('Voir la suite', 'PostsController@show',['id'=>$post->id]) ?>
+                    <?php if (Session::isAuthenticate()): ?>
+                        <?php $Html->addScript('/assets/js/app.js') ?>
+                        <?php if (Session::Auth()->id == $post->user_id) echo $Html->route('Détruire', 'PostsController@destroy',
+                                        ['id'=>$post->id],
+                                        ['class'=>'button alert del-button']); ?>
+                    <?php endif ?>
+                </div>
             </div>
         </article>
     <?php endforeach ?>
