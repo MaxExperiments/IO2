72d257691d4533f1d94bba06622a8e669bd09962
MaxExperiments <maxime.flin@gmail.com>
2017-05-11 21:29:55 +0200

    des oublis

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -8,6 +8,7 @@ class UsersController extends BaseController {
      * Affiche la page de modification de l'utilisateur
      */
     public function index () {
+        // $this->user->sendNotification('Max','Max');
         $this->user->findFirst(Session::Auth()->id);
         App::$response->view('users.index',['user'=>Session::Auth()],['Form'=>[$this->user]]);
     }
@@ -18,6 +19,10 @@ class UsersController extends BaseController {
     public function update () {
         if(empty(App::$request->post['password'])) unset(App::$request->post['password']);
         if(empty(App::$request->post['photo']) || App::$request->post['photo']['error'] == 4) unset(App::$request->post['photo']);
+
+        $this->user->validation['email'][] = 'unique';
+        $this->user->validation['pseudo'][] = 'unique';
+        
         $this->validate ($this->user, App::$request->post);
         App::$request->filterPost();
         if (isset(App::$request->post['photo'])) $this->user->moveFile('photo');
@@ -31,14 +36,17 @@ class UsersController extends BaseController {
      * Affiche la page d'un utilisateur
      * @param  int $id ID de l'utilisateur à afficher
      */
-    public function show ($id) {
+    public function show ($pseudo) {
         $total = $this->post->count();
-        $user = $this->user->findFirst($id);
+        $user = $this->user->where('pseudo',$pseudo)->get();
+        if (empty($user)) throw new NotFoundException("Utilisateur inconnu");
+        $user = $user[0];
+
         $posts = $this->post
                         ->selectFillable()
                         ->order('!coalesce(posts.updated_at, posts.created_at)','DESC')
                         ->order('id','DESC')
-                        ->where('user_id',$id)
+                        ->where('user_id',$user->id)
                         ->paginate()
                         ->get();
         if (empty($user)) throw new NotFoundException ("Aucun utilisateur ne correspond à cet identifiant");
@@ -86,6 +94,8 @@ class UsersController extends BaseController {
      */
     public function store () {
         $this->user->validation['email'][] = 'unique';
+        $this->user->validation['pseudo'][] = 'unique';
+
         if (empty(App::$request->post['photo']['name'])) unset(App::$request->post['photo']);
         $this->validate($this->user, App::$request->post);
         App::$request->filterPost();
--- app/models/User.php
+++ app/models/User.php
@@ -41,4 +41,32 @@ class User extends Model {
         return true;
     }
 
+    public function sendNotification ($subject, $content) {
+        $mail = new PHPMailer;
+
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Host = 'ssl://smtp.gmail.com:465';
+        $mail->Port = 465;                                    // TCP port to connect to
+        $mail->Username = 'maxime.flin@example.com';                 // SMTP username
+        $mail->Password = 'Wd+24Sr7';                           // SMTP password
+
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress('maxime.flin@maxime.flin', 'Maxime Flin');     // Add a recipient
+
+        $mail->isHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        if(!$mail->send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message has been sent';
+        }
+    }
+
 }
